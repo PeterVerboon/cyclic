8e74f17bb60b2e139375ebb34bf09cbbea1bb5eb
Verboon <pve@ou.nl>
2020-03-03 14:47:28 +0100

    vars default set to NULL

--- R/Rdecom.R
+++ R/Rdecom.R
@@ -7,10 +7,10 @@
 #' @param dat data set
 #' @param y dependent variable of the multilevel model
 #' @param grpid group identity (cluster variable)
+#' @param fit lmer or lmerTest object (fit multilevel model)
 #' @param cov covariates in the MLA model
 #' @param xvar1 name of first time variable from the cyclic process
 #' @param xvar2 name of optional second time variable from the cyclic process
-#' @param fit lmer or lmerTest object (fit multilevel model)
 #' @return vector containing three values, named: "level_1_R2","level_2_R2", "total_R2"
 #' @export
 #'
@@ -19,7 +19,7 @@
 #' The undesirable property of negative R-squared values.
 #' Downloaded from \emph{http://arno.uvt.nl/show.cgi?fid=146739}, August 21, 2019.
 #'
-Rdecom <- function(dat, y, grpid,cov,xvar1,xvar2 = NULL, fit) {
+Rdecom <- function(dat, y, grpid, fit, cov = NULL,xvar1 = NULL, xvar2 = NULL) {
 
   # remove missings from the data, otherwise lengths differ
   vars <- c(y, grpid,cov,xvar1,xvar2)
